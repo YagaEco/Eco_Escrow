5725f236bfa7058479112bc2b72b35db2ae3c75e
YagaEco <103841438+YagaEco@users.noreply.github.com>
2022-05-04 17:07:39 +0300

    Update es_com.py

--- commands/es_com.py
+++ commands/es_com.py
@@ -3,23 +3,26 @@ import discord
 import datetime
 from discord.ext import commands, tasks
 from brownie import *
+#connect to need networt Polygon ot other...
 network.connect('matic')
 
 
 class Poll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        #ADD Your ID BOT and Time_delta - time that is given for a meeting in seconds
         self.username_bot = 941283909637926936 ##################ID######################
         self.time_delta = 60 ############AMOUNT SECONDS FOR DEAL#################
-        self.emoji = ['1\u20e3', '2\u20e3', '3\u20e3', '4\u20e3', '5\u20e3',
-                      '6\u20e3', '7\u20e3', '8\u20e3', '9\u20e3', '\U0001F51F']
+
     @commands.Cog.listener()
+    #LOOP
     async def on_ready(self):
         self.poll_result.start()
 
     @commands.Cog.listener()
+
     async def on_message_delete(self, message):
-        # Remove poll
+        # Remove poll if already Done 
         with open('../db/escrow.json', 'r') as file:
             escrow_data = json.load(file)
 
@@ -30,6 +33,9 @@ class Poll(commands.Cog):
                 json.dump(escrow_data, update_poll_data, indent=4)
 
     @commands.command()
+    #Start Deal Escrow 
+    #Example: !escrow 0xa6018d60139fbe7049c2f167d14c57c24eb4d7ac 0xd61749aC5805c84F0934eF8a6aFeF434475B5F42 @noobreckoner 290 10
+    #
     async def escrow(self,ctx, from_:str, to_:str, user: discord.Member, nft:int,price:int): 
         d=0
         author = ctx.message.author.id
@@ -39,12 +45,12 @@ class Poll(commands.Cog):
         #await ctx.send(f'{from_} {to_} {nft}')
         with open('./db/escrow.json', 'r') as poll_file:
             escrow_data = json.load(poll_file)
-
+        # Next comes a check for balances, that is, the correctness of the address WEB3
         try:
             valid_to = web3.eth.getBalance(web3.toChecksumAddress(to_))
             valid_from= web3.eth.getBalance(web3.toChecksumAddress(from_))
 
-
+            #If Deal exist
             for i in escrow_data.values():
                 if str(from_) in i['from'] and str(to_) in i['to']:
                     if str(nft) not in str(i['nft']):
@@ -56,7 +62,7 @@ class Poll(commands.Cog):
                 if str(nft) in str(i['nft']):
                     await ctx.send("NFT already in Deal") 
                     d= 1               
-
+            # Checking against the parameters of our collection may change
             if from_!=to_ and nft>=0 and nft<=359 and d!=1:
                 message = await ctx.send('Ok')
 
@@ -77,7 +83,9 @@ class Poll(commands.Cog):
             await ctx.send('Please check from/to - not_valid')
 
     @commands.command()
+    # transaction verification
     async def check(self,ctx, link): 
+        # Our contract 0x0Ce9a6838F1574F78B84d145df716C6FD3160CA7, previously entered with brownie and abi
         NFT=Contract('0x0Ce9a6838F1574F78B84d145df716C6FD3160CA7')
         with open('./db/escrow.json', 'r') as poll_file:
             escrow_data = json.load(poll_file)
@@ -87,20 +95,17 @@ class Poll(commands.Cog):
         link = link[-1]
 
         tx = web3.eth.getTransaction(link)
-        if tx.to==NFT.address:
+        if tx.to==NFT.address:# checking whether our contract is a transaction
             tx = NFT.decode_input(tx['input'])
             txx = [str(i).lower() for i in tx[1]]
-            #if [escrow_data['from'].lower(),escrow_data['to'].lower(), str(escrow_data['NFT'])]:
-            #    #await ctx.send('TX Complete')
             for i in escrow_data.values():
+                # We verify the parameters of all transactions
                 if [i['from'].lower(),i['to'].lower(),str(i['nft'])]==txx:
-                    if i['payed']==True:
+                    if i['payed']==True: # Additional check for points payment
                         print('TRUE')
-                    # member_obj = awai` ctx.guild.get_member(i['user_id_to'])
-                    # await ctx.send(f'ID to name: {member_obj.display_name}')
+
                         user  = i['user_to_name']
 
-                        # dest = i['dest']
                         print(user)
                         i['transfer_nft']=True
                         with open('./db/escrow.json', 'w') as new_escrow_data:
@@ -109,19 +114,19 @@ class Poll(commands.Cog):
                         await ctx.send(f'TX Complete , {user}')
                     else:
                         await ctx.send(f'Not payed')
-                    #await ctx.send(f'TX Complete , {user}')
-                    #member_obj = user = bot.get_user(i["user_id_to"])
+ 
                 else:
                     await ctx.send(f'Please check valid link')
         else:
             await ctx.send(f'Please check valid link tx')
 
     @commands.command()
+    # send command parsing
     async def send(self,ctx, user, amount:float, id_:int):
-        if user==f'<@&{self.username_bot}>':   
+        if user==f'<@&{self.username_bot}>':  # Checking all submissions to our bot 
             with open('./db/escrow.json', 'r') as poll_file:
                 escrow_data = json.load(poll_file)
-
+            # If it has not been paid before, the amount is checked or more change if less than nothing (the user actually needs to just copy the previous response from the bot)
             if str(id_) in escrow_data and escrow_data[str(id_)]['price']==amount and escrow_data[str(id_)]['payed']==False:
 
                 escrow_data[str(id_)]['payed']=True
@@ -131,6 +136,7 @@ class Poll(commands.Cog):
 
                 await ctx.send(f'Escrow bot get {amount} points from {ctx.author.mention}, deal ID:{id_}')
                 await ctx.send(f'User {escrow_data[str(id_)]["user_to_name"]} please send NFT id : **{escrow_data[str(id_)]["nft"]}** \n from: `{escrow_data[str(id_)]["from"]}` \n to: `{escrow_data[str(id_)]["to"]}`')
+            # In this case, if the user sent more, the bot returns the change
             if str(id_) in escrow_data and escrow_data[str(id_)]['price']>amount and escrow_data[str(id_)]['payed']==False:
                 await ctx.send(f'Please check amount')
                 await ctx.send(f'!send {escrow_data[str(id_)]["user_from_name"]} {amount} refund Deal ID: {escrow_data[str(id_)]["message_id"]}')
@@ -142,7 +148,8 @@ class Poll(commands.Cog):
 
 
                 
-
+            
+            # If less just refund and close the deal
             if str(id_) in escrow_data and escrow_data[str(id_)]['price']<amount and escrow_data[str(id_)]['payed']==False:
                 dif_= amount-escrow_data[str(id_)]['price']
                 print(dif_)
@@ -154,9 +161,6 @@ class Poll(commands.Cog):
 
                 await ctx.send(f'Escrow bot get {amount} points (refund {dif_}) from {ctx.author.mention}, deal ID:{id_}')
                 await ctx.send(f'User {escrow_data[str(id_)]["user_to_name"]} please send NFT id : **{escrow_data[str(id_)]["nft"]}** \n from: `{escrow_data[str(id_)]["from"]}` \n to: `{escrow_data[str(id_)]["to"]}`')
-                # escrow_data.pop([str(id_)])
-                # with open('./db/escrow.json', 'w') as new_escrow_data:
-                #     json.dump(escrow_data, new_escrow_data, indent=4)
 
 
             if str(id_) not in escrow_data:
@@ -181,11 +185,6 @@ class Poll(commands.Cog):
                     json.dump(escrow_data, new_escrow_data, indent=4)
             if int(cur_time)>=int(i['close_time']) and i['transfer_nft']==True and i['payed']==True or i['transfer_nft']==True and i['payed']==True:
                 await channel.send(f'!send {i["user_to_name"]} {i["price"]} success Deal ID: {i["message_id"]}')
-                # with open('./db/escrow_success', 'r') as poll_file:
-                #     escrow_data_ = json.load(poll_file)
-                # escrow_data_[str(i["message_id"])]=i
-                # with open('./db/escrow_success.json', 'w') as escrow_data_log:
-                #     json.dump(escrow_data_, escrow_data_log, indent=4)
 
                 escrow_data.pop(str(message.id))
                 with open('./db/escrow.json', 'w') as new_escrow_data:
@@ -195,8 +194,7 @@ class Poll(commands.Cog):
                 escrow_data.pop(str(message.id))
                 with open('./db/escrow.json', 'w') as new_escrow_data:
                     json.dump(escrow_data, new_escrow_data, indent=4)
-            # if i['transfer_nft']==True and i['payed']==True and int(i['escrow_start_time'])<=int(i['escrow_start_time'])+30:
-            #     await channel.send(f'!send {i["user_to_name"]} {i["price"]} return Deal ID: {i["message_id"]}')
+
 
 
 def setup(bot):
